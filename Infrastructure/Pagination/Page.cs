cfe004e7c36f50c9b3f019ae8862a9e284c00fbf
Vladimir Rudt <vladimir.rudt@saritasa.com>
2021-05-22 06:14:25 +0700

    Add pagination entities

---
+++ Infrastructure/Pagination/Page.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Pagination
+{
+    /// <summary>
+    /// Class for represent page (needed for pagination).
+    /// </summary>
+    /// <typeparam name="T">Type of page content</typeparam>
+    public class Page<T> where T : new()
+    {
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        /// <param name="source">Data source.</param>
+        /// <param name="pageNumber">Page number.</param>
+        /// <param name="pageSize">Page size.</param>
+        /// <param name="cancellationToken">Cancellation token.</param>
+        /// <param name="mapping">Mapping logic.</param>
+        public static async Task<Page<T>> CreatePageAsync(IQueryable<object> source, int pageNumber, int pageSize,
+            CancellationToken cancellationToken, Func<object, T> mapping = null)
+        {
+            var mappingLogic = mapping ?? (obj => (T) obj);
+
+                var sourceItemsCount = source.Count();
+            var total = sourceItemsCount / pageSize + (sourceItemsCount % pageSize != 0 ? 1 : 0);
+
+            var content = await source.Skip((pageNumber - 1) * pageSize)
+                .Take(pageSize)
+                .Select(item => mappingLogic(item))
+                .ToListAsync(cancellationToken);
+
+            return new Page<T>
+            {
+                Total = total,
+                Content = content,
+                PageNumber = pageNumber,
+                PageSize = pageSize
+            };
+        }
+
+        /// <summary>
+        /// Page content.
+        /// </summary>
+        public IList<T> Content { get; init; }
+
+        /// <summary>
+        /// Total number of pages.
+        /// </summary>
+        public int Total { get; init; }
+
+        /// <summary>
+        /// Current page.
+        /// </summary>
+        public int PageNumber { get; init; }
+
+        /// <summary>
+        /// Size of page.
+        /// </summary>
+        public int PageSize { get; init; }
+    }
+}
---
+++ Infrastructure/Pagination/PageRequest.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using MediatR;
+
+namespace Infrastructure.Pagination
+{
+    /// <summary>
+    /// Base class for queries that returns content as page.
+    /// </summary>
+    public abstract class PagedRequest<T> : IRequest<Page<T>> where T : new()
+    {
+        /// <summary>
+        /// Pagination number.
+        /// </summary>
+        [Range(1, int.MaxValue)]
+        public int PageNumber { get; init; } = 1;
+
+        /// <summary>
+        /// Page size.
+        /// </summary>
+        [Range(1, 60)]
+        public int PageSize { get; init; } = 10;
+    }
+}
