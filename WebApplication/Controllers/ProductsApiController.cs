17c4e59cd67d83c53dd9182358610ff0d7fa701d
Vladimir Rudt <vladimir.rudt@saritasa.com>
2021-05-22 06:28:59 +0700

    Add routing for product by id

--- WebApplication/Controllers/ProductsApiController.cs
+++ WebApplication/Controllers/ProductsApiController.cs
@@ -3,6 +3,7 @@ using System.Threading.Tasks;
 using Infrastructure.Pagination;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
+using UseCases.Products.GetProductById;
 using UseCases.Products.GetProducts;
 using UseCases.Products.GetProducts.Dtos;
 
@@ -30,6 +31,19 @@ namespace WebApplication.Controllers
         /// <returns>Page with products.</returns>
         [HttpGet]
         public async Task<Page<ProductDto>> GetProducts([FromQuery]GetProductsQuery query, CancellationToken cancellationToken)
+        {
+            return await _mediator.Send(query, cancellationToken);
+        }
+
+        /// <summary>
+        /// Get product by id.
+        /// </summary>
+        /// <param name="query">Get products query.</param>
+        /// <param name="cancellationToken">Cancellation token.</param>
+        /// <returns>Required product.</returns>
+        [HttpGet("{ProductId}")]
+        public async Task<ProductDto> GetProductById([FromRoute]GetProductByIdQuery query,
+            CancellationToken cancellationToken)
         {
             return await _mediator.Send(query, cancellationToken);
         }
