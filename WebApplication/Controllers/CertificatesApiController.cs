2eca42fe745897c2239da717872375bfb534ab95
Vladimir Rudt <vladimir.rudt@saritasa.com>
2021-05-29 11:49:06 +0700

    Add query for all certificates

---
+++ WebApplication/Controllers/CertificatesApiController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Domain;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using UseCases.Categories.GetCategories;
+using UseCases.Categories.GetCategory;
+using UseCases.Certificates.GetCertificates;
+using UseCases.Products.GetProducts.Dtos;
+
+namespace WebApplication.Controllers
+{
+    /// <summary>
+    /// Controller for certificates.
+    /// </summary>
+    [ApiController]
+    [Route("api/v1/certificates")]
+    public class CertificatesApiController
+    {
+        private readonly IMediator _mediator;
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        /// <param name="mediator">MediatR object.</param>
+        public CertificatesApiController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        /// <summary>
+        /// Returns all certificates.
+        /// </summary>
+        /// <param name="query">Request for all certificates.</param>
+        /// <param name="cancellationToken">Cancellation token.</param>
+        [HttpGet]
+        public async Task<IEnumerable<Certificate>> GetAllCategories([FromQuery]GetCertificatesQuery query, CancellationToken cancellationToken)
+        {
+            return await _mediator.Send(query, cancellationToken);
+        }
+    }
+}
