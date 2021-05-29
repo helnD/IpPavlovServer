aa23a308c1bca6b906f46d93bbaa82fb43da21ff
Vladimir Rudt <vladimir.rudt@saritasa.com>
2021-05-29 14:37:46 +0700

    Add service for email sending

---
+++ Infrastructure.Abstractions/IEmailSender.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Abstractions
+{
+    /// <summary>
+    /// Interface that provides functionality for email sending.
+    /// </summary>
+    public interface IEmailSender
+    {
+        /// <summary>
+        /// Send question.
+        /// </summary>
+        /// <param name="name">Name of user who send question.</param>
+        /// <param name="email">Email of sender.</param>
+        /// <param name="question">Question text.</param>
+        /// <param name="cancellationToken">Cancellation token.</param>
+        Task SendQuestionAsync(string name, string email, string question, CancellationToken cancellationToken);
+    }
+}
